c2f8852485f84a817847be500e53bf016139783a
Max Waterman <davidmaxwaterman@fastmail.co.uk>
2021-12-20 20:21:16 +0800

    wip

--- src/DmwAnnexLicencePage.test.js
+++ src/DmwAnnexLicencePage.test.js
@@ -2,23 +2,32 @@ import { render, screen } from '@testing-library/react';
 import DmwAnnexLicencePage from './DmwAnnexLicencePage';
 
 test('renders licence page without crashing', () => {
-  render(<DmwAnnexLicencePage />);
+    render( < DmwAnnexLicencePage / > );
 });
 
 test('scroller exists', () => {
-  const dom = render(<DmwAnnexLicencePage />);
-  const {container} = dom;
-  const scroller = container.querySelector('.scroller');
+    const dom = render( < DmwAnnexLicencePage / > );
+    const { container } = dom;
+    const scroller = container.querySelector('.scroller');
 
-  expect(scroller).toBeInTheDocument();
+    expect(scroller).toBeInTheDocument();
+});
+
+test('scroller fills viewport', () => {
+    const dom = render( < DmwAnnexLicencePage / > );
+    const { container } = dom;
+    const scroller = container.querySelector('.scroller');
+
+    screen.debug();
+
+    expect(scroller).toBeInTheDocument();
 });
 
 test('back anchor exists', () => {
-  const dom = render(<DmwAnnexLicencePage />);
-  const {container} = dom;
-  const back = screen.getByText(/back/i);
+    render( < DmwAnnexLicencePage / > );
 
-  expect(back).toBeInTheDocument();
-  expect(back.getAttribute('href')).toEqual('/');
+    const back = screen.getByText(/back/i);
+
+    expect(back).toBeInTheDocument();
+    expect(back.getAttribute('href')).toEqual('/');
 });
-
